71a4b2438df5d4273b20fdd8eca8ec2e94972038
Philippe Kimura-Thollander <philippekimurathollander@gmail.com>
2017-10-15 18:23:35 -0400

    add navbar scroll

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,12 +1,21 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import Headroom from 'react-headroom';
 import './Navbar.css';
 
 class Navbar extends Component {
 
+  constructor() {
+    super();
+
+    this.state = {
+      visible: true
+    }
+  }
+
   static propTypes = {
     nextProjectLink: PropTypes.string.isRequired,
-    nextProjectName: PropTypes.string.isRequired
+    nextProjectName: PropTypes.string.isRequired,
   }
 
   static defaultProps = {
@@ -18,10 +27,15 @@ class Navbar extends Component {
   render() {
 
     return (
-      <navbar className="navbar">
-        <a href="/">Home</a>
-        <a href="/">Next Project</a>
-      </navbar>
+      <Headroom
+        onPin={() => console.log('pinned')}
+        onUnpin={() => console.log('unpinned')}
+      >
+        <navbar className="navbar">
+          <a href="/">Home</a>
+          <a href="/">Next Project</a>
+        </navbar>
+      </Headroom>
     );
   }
 }
